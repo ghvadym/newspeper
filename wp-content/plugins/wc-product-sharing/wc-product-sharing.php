5b0b9d33d8229fbf51c016709a78f281c6366c0c
Vadym Kravchenko <vkravchenko2244@gmail.com>
2022-04-08 19:24:24 +0300

    new commit

---
+++ wp-content/plugins/wc-product-sharing/wc-product-sharing.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin name: FX WC Product Sharing
+Description: Connect with API and use woocommerce products.
+Author: Flexi
+Text Domain: wcapi
+Version 1.0
+ */
+
+if (!defined('ABSPATH')) exit;
+
+define('PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('PLUGIN_URL', plugin_dir_url(__FILE__));
+define('PLUGIN_NAME', plugin_basename(__DIR__));
+define('THEME_PATH', get_template_directory());
+define('THEME_URL', get_template_directory_uri());
